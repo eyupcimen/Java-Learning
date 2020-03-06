e84da8520320f3db3015a4742a58b041f638a6de
eyupcimen <cmneyp@gmail.com>
2020-03-06 15:05:29 +0300

    initial commit

---
+++ src/Helloworld.java
@@ -0,0 +1,28 @@
+
+import java.util.Date; 
+
+public class Helloworld {
+
+	
+	public static void main(String[] args) {
+		
+		
+		//byte age = 30;
+		//long viewsCount = 232123_456_789L ;
+		//float price = 10.99F;
+		//char letter = 'A';
+		//boolean isEligible = false ; 
+		
+		//var now = new Date();
+		//System.out.println(now);
+				
+		byte x = 1;
+		byte y = x;
+		
+		x = 2;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+	} 
+}
---
+++ src/SecondlyClass.java
@@ -0,0 +1,8 @@
+
+public class SecondlyClass {
+
+	@Override
+	public String toString() {
+		return "SecondlyClass benim yazdigim";
+	}
+}
