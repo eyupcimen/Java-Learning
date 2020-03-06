5ef1c3fe5d588f66d73700d7220de8022348efe7
eyupcimen <cmneyp@gmail.com>
2020-03-06 16:21:01 +0300

    Made constant any parameter using final keyword

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -4,15 +4,9 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		 
-		//int[][][] numbers = new int[2][3][5];
-		//numbers[0][0][0] = 3;
-		//System.out.println(Arrays.deepToString(numbers));
 		
-		int[][] numbers = { {1,2,3} , {4,5,6} };
-		
-		numbers[0][0] = 3;
-		System.out.println(Arrays.deepToString(numbers));
-		
+		final float PI = 3.14F;
+		System.out.println(PI);
 		
 	} 
 }
