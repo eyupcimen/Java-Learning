a7e99823881b5bdbba26a3a373a37546607b431d
eyupcimen <cmneyp@gmail.com>
2020-03-06 16:06:31 +0300

    How to use with array.

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,39 +1,15 @@
+import java.util.Arrays;
+
 public class Helloworld {
 
 	public static void main(String[] args) {
 		 
-		String messageString = "Hello World" + "!!!";
+		int[] numbers = {2, 7, 3, 8, 1, 4 };
 		
+		Arrays.sort(numbers);
 		
-		System.out.println(messageString.startsWith("Hello"));
-		System.out.println(messageString.endsWith("!!"));	
+		System.out.println(numbers.length);
 		
-		System.out.println(messageString.length());	
-		
-		System.out.println(messageString.indexOf("W"));
-		
-		System.out.println(messageString.indexOf("w"));
-		System.out.println(messageString.indexOf("sky"));
-		
-		System.out.println(messageString.replace("d","A"));
-		System.out.println(messageString.replaceAll("!", "*") );
-		
-		System.out.println(messageString);
-		
-		System.out.println(messageString.toLowerCase());
-		System.out.println(messageString.toUpperCase());
-		
-		
-		String exampleMessageforTrim= "    Hello World For Trim      *" + "*    !!!    ";
-		System.out.println(exampleMessageforTrim.trim());
-		
-		
-		String newMessage = "Hello \"Mosh\"";
-		System.out.println(newMessage);
-		
-		// c:\Windows\...
-		String newPassingBackSlash = "c:\\Windows\\... \nnew line \ttab using";
-		System.out.println(newPassingBackSlash);
-		
+		System.out.println(Arrays.toString(numbers));
 	} 
 }
