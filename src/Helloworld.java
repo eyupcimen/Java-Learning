ba88e6b0c2debf2e603a355455f639eb3e59f77c
eyupcimen <cmneyp@gmail.com>
2020-03-06 15:56:51 +0300

    How to use with string. Start character, end character, lenght, upper case, trim and more than

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,15 +1,39 @@
-
-import java.awt.*;
-
 public class Helloworld {
 
 	public static void main(String[] args) {
 		 
+		String messageString = "Hello World" + "!!!";
 		
-		Point point1 = new Point(1, 1);
-		Point point2 = point1;
-		point1.x = 3;
 		
-		System.out.println(point2);
+		System.out.println(messageString.startsWith("Hello"));
+		System.out.println(messageString.endsWith("!!"));	
+		
+		System.out.println(messageString.length());	
+		
+		System.out.println(messageString.indexOf("W"));
+		
+		System.out.println(messageString.indexOf("w"));
+		System.out.println(messageString.indexOf("sky"));
+		
+		System.out.println(messageString.replace("d","A"));
+		System.out.println(messageString.replaceAll("!", "*") );
+		
+		System.out.println(messageString);
+		
+		System.out.println(messageString.toLowerCase());
+		System.out.println(messageString.toUpperCase());
+		
+		
+		String exampleMessageforTrim= "    Hello World For Trim      *" + "*    !!!    ";
+		System.out.println(exampleMessageforTrim.trim());
+		
+		
+		String newMessage = "Hello \"Mosh\"";
+		System.out.println(newMessage);
+		
+		// c:\Windows\...
+		String newPassingBackSlash = "c:\\Windows\\... \nnew line \ttab using";
+		System.out.println(newPassingBackSlash);
+		
 	} 
 }
