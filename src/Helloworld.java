678f6b1762574cfe93c26ed1e0936e5dcd57a6f1
eyupcimen <cmneyp@gmail.com>
2020-03-06 15:17:36 +0300

    Explains the difference between "primitive" type and "reference" type

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,28 +1,15 @@
 
-import java.util.Date; 
+import java.awt.*;
 
 public class Helloworld {
 
-	
 	public static void main(String[] args) {
+		 
 		
+		Point point1 = new Point(1, 1);
+		Point point2 = point1;
+		point1.x = 3;
 		
-		//byte age = 30;
-		//long viewsCount = 232123_456_789L ;
-		//float price = 10.99F;
-		//char letter = 'A';
-		//boolean isEligible = false ; 
-		
-		//var now = new Date();
-		//System.out.println(now);
-				
-		byte x = 1;
-		byte y = x;
-		
-		x = 2;
-		
-		System.out.println(x);
-		System.out.println(y);
-		
+		System.out.println(point2);
 	} 
 }
--- src/SecondlyClass.java
+++
@@ -1,8 +0,0 @@
-
-public class SecondlyClass {
-
-	@Override
-	public String toString() {
-		return "SecondlyClass benim yazdigim";
-	}
-}
