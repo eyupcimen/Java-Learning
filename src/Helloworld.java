9777a159f2efaab2246bd4e7922e98b99bdb0640
eyupcimen <cmneyp@gmail.com>
2020-04-05 21:07:55 +0300

    Reading Inuput

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,21 +1,10 @@
-import java.text.NumberFormat;
+import com.sun.java_cup.internal.runtime.Scanner;
 
 public class Helloworld {
 	
 	public static void main(String[] args) {
 	 
-		
-		NumberFormat currency = NumberFormat.getCurrencyInstance();
-		String resultCurrency = currency.format(123456.5435);
-		System.out.println(resultCurrency);
-		
-		
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		String resultPercent = percent.format(0.1);
-		System.out.println(resultPercent);
-		
-		
-		String shortVersion = NumberFormat.getPercentInstance().format(0.19);
-		System.out.println(shortVersion);
+		Scanner scanner = new Scanner(System.in);
+		scanner.next
 	}
 }
